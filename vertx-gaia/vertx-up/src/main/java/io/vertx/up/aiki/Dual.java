d90d9b4ac800dcce700074bcf605b70480e03c8b
Lang <silentbalanceyh@126.com>
2018-01-11 07:20:03 +0800

    Ux append method for (Object,Array)

--- vertx-gaia/vertx-up/src/main/java/io/vertx/up/aiki/Dual.java
+++ vertx-gaia/vertx-up/src/main/java/io/vertx/up/aiki/Dual.java
@@ -1,11 +1,12 @@
 package io.vertx.up.aiki;
 
 import io.reactivex.Observable;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
 class Dual {
 
-    static JsonObject merge(
+    static JsonObject append(
             final JsonObject target,
             final JsonObject source,
             final boolean immutable
@@ -15,5 +16,15 @@ class Dual {
                 .filter(key -> !target.containsKey(key))
                 .subscribe(key -> result.put(key, source.getValue(key)));
         return result;
+    }
+
+    static JsonObject append(
+            final JsonObject target,
+            final JsonArray sources
+    ) {
+        Observable.fromIterable(sources)
+                .map(item -> (JsonObject) item)
+                .subscribe(item -> append(target, item, false));
+        return target;
     }
 }

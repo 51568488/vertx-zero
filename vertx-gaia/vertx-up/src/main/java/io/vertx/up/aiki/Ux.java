e88443d65ecf6eb1f554b5e635bb1b74d6a3175e
Lang <silentbalanceyh@126.com>
2018-01-11 09:35:48 +0800

    Blade added

--- vertx-gaia/vertx-up/src/main/java/io/vertx/up/aiki/Ux.java
+++ vertx-gaia/vertx-up/src/main/java/io/vertx/up/aiki/Ux.java
@@ -19,34 +19,6 @@ import java.util.function.Supplier;
  */
 @SuppressWarnings("unchecked")
 public final class Ux {
-    /**
-     * Json calculation
-     */
-    public static class Json {
-
-        public static JsonObject deNull(final JsonObject entity) {
-            return Self.deNull(entity, false);
-        }
-
-        public static JsonObject deNullNew(final JsonObject entity) {
-            return Self.deNull(entity, true);
-        }
-
-        public static JsonObject append(final JsonObject target, final JsonObject source) {
-            return Dual.append(target, source, false);
-        }
-
-        /**
-         * Because of Array, do not support immutable
-         */
-        public static JsonObject append(final JsonObject target, final JsonArray array) {
-            return Dual.append(target, array);
-        }
-
-        public static JsonObject appendNew(final JsonObject target, final JsonObject source) {
-            return Dual.append(target, source, true);
-        }
-    }
 
     public static <T> JsonObject toJson(final T entity) {
         return To.toJson(entity, "");

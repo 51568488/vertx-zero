a3720c358e3c433dd0a8ad0437d2d685623d4e07
Lang <silentbalanceyh@126.com>
2018-01-14 18:21:16 +0800

    Tools for remove

--- vertx-gaia/vertx-up/src/main/java/io/vertx/up/aiki/ZERO.java
+++ vertx-gaia/vertx-up/src/main/java/io/vertx/up/aiki/ZERO.java
@@ -5,4 +5,5 @@ interface Info {
     String MSG_FILTER = "[ ZERO ] ( Mongo -> findOne ) collection = {0}, filter = {1}, result = {2}.";
     String MSG_INSERT = "[ ZERO ] ( Mongo -> insert ) collection = {0}, data = {1}.";
     String MSG_UPDATE = "[ ZERO ] ( Mongo -> findOneAndReplace ) collection = {0}, filter = {1}, result = {2}.";
+    String MSG_DELETE = "[ ZERO ] ( Mongo -> removeDocument ) Effected: {2} Rows. collection = {0}, filter = {1}";
 }

878c8427f61d70c2fc6bec3c26d6ac4a0d05fd44
Lang <silentbalanceyh@126.com>
2018-01-14 23:33:36 +0800

    Fix issue of Uson

--- vertx-gaia/vertx-co/src/main/java/io/vertx/up/aiki/Self.java
+++ vertx-gaia/vertx-co/src/main/java/io/vertx/up/aiki/Self.java
@@ -5,6 +5,7 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.up.tool.StringUtil;
 
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentMap;
@@ -46,17 +47,18 @@ class Self {
             final ConcurrentMap<String, String> mapping,
             final boolean immutable
     ) {
-        final JsonObject result = immutable ? entity.copy() : entity;
+        final JsonObject result = new JsonObject();
+        final Set<String> keys = new HashSet<>();
         for (final String from : entity.fieldNames()) {
             // Find to field
             // JsonArray loop
-            final Object value = result.getValue(from);
+            final Object value = entity.getValue(from);
             if (null == value) {
                 // null pointer
                 if (mapping.containsKey(from)) {
                     final String to = mapping.get(from);
                     result.put(to, value);
-                    result.remove(from);
+                    keys.add(from);
                 }
             } else {
                 if (JsonArray.class == value.getClass()) {
@@ -70,12 +72,17 @@ class Self {
                     if (mapping.containsKey(from)) {
                         final String to = mapping.get(from);
                         result.put(to, value);
-                        result.remove(from);
+                        keys.add(from);
                     }
                 }
             }
         }
-        return result;
+        keys.forEach(key -> {
+            // Remove all keys from different JsonObject
+            entity.remove(key);
+            result.remove(key);
+        });
+        return immutable ? result : entity.mergeIn(result, true);
     }
 
     static JsonArray convert(

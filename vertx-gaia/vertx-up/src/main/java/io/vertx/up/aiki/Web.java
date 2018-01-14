31bf04aa22e4895424e15fc560b1caed933d2f21
Lang <silentbalanceyh@126.com>
2018-01-14 16:52:00 +0800

    Ux tool for Mongo

--- vertx-gaia/vertx-up/src/main/java/io/vertx/up/aiki/Web.java
+++ vertx-gaia/vertx-up/src/main/java/io/vertx/up/aiki/Web.java
@@ -3,12 +3,12 @@ package io.vertx.up.aiki;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonObject;
 import io.vertx.up.atom.Envelop;
 
 class Web {
 
     static <T> Handler<AsyncResult<T>> toHandler(
-            final Class<?> clazz,
             final Message<Envelop> message
     ) {
         return handler -> {
@@ -16,7 +16,20 @@ class Web {
                 message.reply(To.toEnvelop(handler.result()));
             } else {
                 // Readible codec for configured information, error flow needed.
-                message.reply(Envelop.failure(To.toError(clazz, handler.cause())));
+                message.reply(Envelop.failure(To.toError(Web.class, handler.cause())));
+            }
+        };
+    }
+
+    static <T> Handler<AsyncResult<Boolean>> toHandler(
+            final Message<Envelop> message,
+            final JsonObject data
+    ) {
+        return handler -> {
+            if (handler.succeeded() && handler.result()) {
+                message.reply(To.toEnvelop(data));
+            } else {
+                message.reply(To.toEnvelop(Boolean.FALSE));
             }
         };
     }
